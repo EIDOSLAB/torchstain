4edf82a6688b27fe8e60c8780e74d21532d199f0
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-12-22 16:58:47 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
+import pathlib
 from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 setup(
     name='torchstain',
     version='1.0',
-    description='pytorch stain normalization utils',
-    url='git@github.com:EIDOSlab/torchstain.git',
-    author='Carlo Alberto Barbano',
+    description='Pytorch stain normalization utils',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/EIDOSlab/torchstain',
+    author='EIDOSlab',
     author_email='carlo.alberto.barbano@outlook.com',
     license='MIT',
     packages=find_packages(exclude=('tests')),
