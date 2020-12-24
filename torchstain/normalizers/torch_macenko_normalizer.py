47c8889daffab93b8b72c857afc6fd3a2dfa7415
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-12-24 17:54:26 +0100

    minor updates

--- torchstain/normalizers/torch_macenko_normalizer.py
+++ torchstain/normalizers/torch_macenko_normalizer.py
@@ -1,5 +1,5 @@
 import torch
-
+import cv2
 from torchstain.normalizers.he_normalizer import HENormalizer
 from torchstain.utils import cov, percentile
 
@@ -12,13 +12,15 @@ class TorchMacenkoNormalizer(HENormalizer):
         super().__init__()
 
         self.HERef = torch.tensor([[0.5626, 0.2159],
-                          [0.7201, 0.8012],
-                          [0.4062, 0.5581]])
+                                   [0.7201, 0.8012],
+                                   [0.4062, 0.5581]])
         self.maxCRef = torch.tensor([1.9705, 1.0308])
 
     def __convert_rgb2od(self, I, Io, beta):
+        I = I.permute(1, 2, 0)
+
         # calculate optical density
-        OD = -torch.log((I.float()+1)/Io)
+        OD = -torch.log((I.reshape((-1, I.shape[-1])).float()+1)/Io)
 
         # remove transparent pixels
         ODhat = OD[~torch.any(OD < beta, dim=1)]
@@ -28,46 +30,43 @@ class TorchMacenkoNormalizer(HENormalizer):
     def __find_HE(self, ODhat, eigvecs, alpha):
         # project on the plane spanned by the eigenvectors corresponding to the two
         # largest eigenvalues
-        That = torch.matmul(ODhat, eigvecs[:, 1:3])
-        phi = torch.atan2(That[:,1],That[:,0])
+        That = torch.matmul(ODhat, eigvecs)
+        phi = torch.atan2(That[:, 1], That[:, 0])
 
-        minPhi = percentile(phi, alpha)
-        maxPhi = percentile(phi, 100-alpha)
+        minPhi = torch.tensor(percentile(phi, alpha))
+        maxPhi = torch.tensor(percentile(phi, 100 - alpha))
 
-        vMin = torch.matmul(eigvecs[:,1:3], torch.tensor([(torch.cos(torch.tensor(minPhi))), torch.sin(torch.tensor(minPhi))]).T).unsqueeze(1)
-        vMax = torch.matmul(eigvecs[:,1:3], torch.tensor([(torch.cos(torch.tensor(maxPhi))), torch.sin(torch.tensor(maxPhi))]).T).unsqueeze(1)
+        vMin = torch.matmul(eigvecs, torch.stack((torch.cos(minPhi), torch.sin(minPhi))).T).unsqueeze(1)
+        vMax = torch.matmul(eigvecs, torch.stack((torch.cos(maxPhi), torch.sin(maxPhi))).T).unsqueeze(1)
 
         # a heuristic to make the vector corresponding to hematoxylin first and the
         # one corresponding to eosin second
         if vMin[0] > vMax[0]:
-            HE = torch.stack((vMin[:,0], vMax[:,0])).T
+            HE = torch.cat((vMin, vMax), dim=1)
+
         else:
-            HE = torch.stack((vMax[:,0], vMin[:,0])).T
+            HE = torch.cat((vMax, vMin), dim=1)
 
         return HE
 
     def __find_concentration(self, OD, HE):
         # rows correspond to channels (RGB), columns to OD values
-        Y = OD.reshape(-1, 3).T
+        Y = OD.T
 
         # determine concentrations of the individual stains
-        C = torch.lstsq(Y, HE)[0][:2]
-        return C
+        return torch.lstsq(Y, HE)[0][:2]
 
     def __compute_matrices(self, I, Io, alpha, beta):
-        I = I.permute(1, 2, 0)
-        I = I.reshape((-1, I.shape[-1]))
-
         OD, ODhat = self.__convert_rgb2od(I, Io=Io, beta=beta)
 
         # compute eigenvectors
-        _, eigvecs = torch.eig(cov(ODhat.T), eigenvectors=True)
-        eigvecs = -torch.flip(eigvecs, dims=(1,))
+        _, eigvecs = torch.symeig(cov(ODhat.T), eigenvectors=True)
+        eigvecs = eigvecs[:, [1, 2]]
 
         HE = self.__find_HE(ODhat, eigvecs, alpha)
 
         C = self.__find_concentration(OD, HE)
-        maxC = torch.tensor([percentile(C[0,:], 99), percentile(C[1,:], 99)])
+        maxC = torch.tensor([percentile(C[0, :], 99), percentile(C[1, :], 99)])
 
         return HE, C, maxC
 
@@ -104,22 +103,21 @@ class TorchMacenkoNormalizer(HENormalizer):
         HE, C, maxC = self.__compute_matrices(I, Io, alpha, beta)
 
         # normalize stain concentrations
-        maxC = maxC / self.maxCRef
-        C2 = C / maxC.unsqueeze(-1)
+        C *= (self.maxCRef / maxC).unsqueeze(-1)
 
         # recreate the image using reference mixing matrix
-        Inorm = Io * torch.exp(-torch.matmul(self.HERef, C2))
+        Inorm = Io * torch.exp(-torch.matmul(self.HERef, C))
         Inorm[Inorm > 255] = 254
         Inorm = Inorm.T.reshape(h, w, c).int()
 
         H, E = None, None
 
         if stains:
-            H = torch.mul(Io, torch.exp(torch.matmul(-self.HERef[:, 0].unsqueeze(-1), C2[0, :].unsqueeze(0))))
+            H = torch.mul(Io, torch.exp(torch.matmul(-self.HERef[:, 0].unsqueeze(-1), C[0, :].unsqueeze(0))))
             H[H > 255] = 254
             H = H.T.reshape(h, w, c).int()
 
-            E = torch.mul(Io, torch.exp(torch.matmul(-self.HERef[:, 1].unsqueeze(-1), C2[1, :].unsqueeze(0))))
+            E = torch.mul(Io, torch.exp(torch.matmul(-self.HERef[:, 1].unsqueeze(-1), C[1, :].unsqueeze(0))))
             E[E > 255] = 254
             E = E.T.reshape(h, w, c).int()
 
