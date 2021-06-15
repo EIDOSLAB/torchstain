da4ade608a11fb373119d3bc726d79193fa3eee5
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2021-06-15 11:18:10 +0200

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='torchstain',
-    version='1.0.1',
+    version='1.1.0',
     description='Pytorch stain normalization utils',
     long_description=README,
     long_description_content_type='text/markdown',
