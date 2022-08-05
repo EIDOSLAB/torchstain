518a77b7d95c608819ecfd0af1e779b1f072ac93
andreped <andrped94@gmail.com>
2022-08-05 09:54:20 +0200

    bug fix normalizer test

--- tests/test_normalizers.py
+++ tests/test_normalizers.py
@@ -6,13 +6,13 @@ import torch
 from torchvision import transforms
 import time
 from skimage.metrics import structural_similarity as ssim
+import numpy as np
 
 
 size = 1024
 curr_file_path = os.path.dirname(os.path.realpath(__file__))
-print("dir path:", curr_file_path)
-target = cv2.resize(cv2.cvtColor(cv2.imread(curr_file_path + "../data/target.png"), cv2.COLOR_BGR2RGB), (size, size))
-to_transform = cv2.resize(cv2.cvtColor(cv2.imread(curr_file_path + "../data/source.png"), cv2.COLOR_BGR2RGB), (size, size))
+target = cv2.resize(cv2.cvtColor(cv2.imread(os.path.join(curr_file_path, "../data/target.png")), cv2.COLOR_BGR2RGB), (size, size))
+to_transform = cv2.resize(cv2.cvtColor(cv2.imread(os.path.join(curr_file_path, "../data/source.png")), cv2.COLOR_BGR2RGB), (size, size))
 
 # setup preprocessing and preprocess image to be normalized
 T = transforms.Compose([
@@ -36,10 +36,11 @@ result_numpy, _, _ = normalizer.normalize(I=to_transform, stains=True)
 result_torch, _, _ = torch_normalizer.normalize(I=t_to_transform, stains=True)
 result_tf, _, _ = tf_normalizer.normalize(I=t_to_transform, stains=True)
 
-# calculate SSIM to use as metric for assessment if the results are similar
-print(ssim(result_numpy, result_torch))
-print(ssim(result_numpy, result_tf))
+# convert to numpy and set dtype
+result_numpy = result_numpy.astype("float32")
+result_torch = result_torch.numpy().astype("float32")
+result_tf = result_tf.numpy().astype("float32")
 
 # assess whether the normalized images are identical across backends
-np.testing.assert_equal(result_numpy, result_torch, verbose=True)
-np.testing.assert_equal(result_numpy, result_tf, verbose=True)
+np.testing.assert_almost_equal(ssim(result_numpy.flatten(), result_torch.flatten()), 1.0, decimal=4, verbose=True)
+np.testing.assert_almost_equal(ssim(result_numpy.flatten(), result_tf.flatten()), 1.0, decimal=4, verbose=True)
