cd9d1cc6c4a0213acc11aa725c90ee71adfe38e1
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2022-08-05 19:54:36 +0200

    remove unnecessary import

--- torchstain/__init__.py
+++ torchstain/__init__.py
@@ -1,17 +1,3 @@
 __version__ = '1.1.0'
-#__BACKEND__ = 'torch'
 
-#import os
-#os.environ['backend'] = "torch"
-
-from torchstain.base.normalizers.macenko_normalizer import MacenkoNormalizer
-
-try:
-    import torch
-    from torchstain.torch_backend.normalizers.torch_macenko_normalizer import TorchMacenkoNormalizer
-except ModuleNotFoundError:
-    try:
-        import tensorflow
-        from torchstain.tf_backend.normalizers.tensorflow_macenko_normalizer import TensorFlowMacenkoNormalizer
-    except ModuleNotFoundError:
-        pass
+from torchstain.base import normalizers
