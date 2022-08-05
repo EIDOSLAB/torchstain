23ef48120d871b582310fefccb14567b3a29cd13
andreped <andrped94@gmail.com>
2022-08-05 17:14:32 +0200

    from module in test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,7 +5,7 @@ import numpy as np
 def test_cov():
     x = np.random.randn(10, 10)
     cov_np = np.cov(x)
-    cov_t = torchstain.torch.utils.cov(torch.tensor(x))
+    cov_t = torchstain.torch_backend.utils.cov(torch.tensor(x))
 
     np.testing.assert_almost_equal(cov_np, cov_t.numpy())
 
@@ -13,6 +13,6 @@ def test_percentile():
     x = np.random.randn(10, 10)
     p = 20
     p_np = np.percentile(x, p, interpolation='nearest')
-    p_t = torchstain.torch.utils.percentile(torch.tensor(x), p)
+    p_t = torchstain.torch_backend.utils.percentile(torch.tensor(x), p)
 
     np.testing.assert_almost_equal(p_np, p_t)
