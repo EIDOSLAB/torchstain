fixed tf percentile test - off by one

tests/test_tf.py
@@ -18,10 +18,9 @@ def test_percentile():
     x = np.random.randn(10, 10)
     p = 20
     p_np = np.percentile(x, p, interpolation='nearest')
-    p_t = torchstain.tf.utils.percentile(x, p)
+    p_t = torchstain.tf.utils.percentile(tf.convert_to_tensor(x), p)
 
     np.testing.assert_almost_equal(p_np, p_t)
-
 
 def test_normalize_tf():
     size = 1024

torchstain/tf/utils/percentile.py
@@ -14,5 +14,5 @@ def percentile(t: tf.Tensor, q: float) -> Union[int, float]:
     :param q: Percentile to compute, which must be between 0 and 100 inclusive.
     :return: Resulting value (scalar).
     """
-    k = 1 + tf.math.round(.01 * tf.cast(q, tf.float32) * (tf.cast(tf.size(t), tf.float32) - 1))
-    return tf.sort(tf.reshape(t, [-1]))[tf.cast(k, tf.int32)]
+    k = 1 + tf.math.round(.01 * tf.cast(q, tf.float32) * (tf.cast(tf.math.reduce_prod(tf.size(t)), tf.float32) - 1))
+    return tf.sort(tf.reshape(t, [-1]))[tf.cast(k - 1, tf.int32)]