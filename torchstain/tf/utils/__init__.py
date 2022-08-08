5d1b729f1e8cc98a700ce2ab1679d958edfe1b3e
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2022-08-08 21:23:02 +0200

    move solveLS to torchstain.tf.utils

--- torchstain/tf/utils/__init__.py
+++ torchstain/tf/utils/__init__.py
@@ -1,3 +1,3 @@
 from torchstain.tf.utils.cov import cov
 from torchstain.tf.utils.percentile import percentile
-from torchstain.tf.normalizers.solveLS import solveLS
+from torchstain.tf.utils.solveLS import solveLS
