a4246c26ec9aed2f514849f013b7d78ec2d11071
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2022-08-09 13:14:41 +0200

    update description

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 setup(
     name='torchstain',
     version='1.2.0',
-    description='Pytorch stain normalization utils',
+    description='Stain normalization utils for pytorch and tensorflow',
     long_description=README,
     long_description_content_type='text/markdown',
     url='https://github.com/EIDOSlab/torchstain',
