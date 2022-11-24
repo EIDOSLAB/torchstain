fbf06275ca3c7392659a26773f2619ddab829bed
André Pedersen <andrped94@gmail.com>
2022-11-24 12:07:03 +0100

    added RGB/LAB converters for TF

--- torchstain/tf/utils/__init__.py
+++ torchstain/tf/utils/__init__.py
@@ -1,3 +1,7 @@
 from torchstain.tf.utils.cov import cov
 from torchstain.tf.utils.percentile import percentile
 from torchstain.tf.utils.solveLS import solveLS
+from torchstain.tf.utils.stats import *
+from torchstain.tf.utils.split import *
+from torchstain.tf.utils.rgb2lab import *
+from torchstain.tf.utils.lab2rgb import *
