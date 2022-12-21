a0f7cf0793672b972af9f0d663b80db34aaf599b
André Pedersen <andrped94@gmail.com>
2022-12-21 17:59:44 +0100

    numpy standardize brightness

--- torchstain/numpy/utils/stats.py
+++ torchstain/numpy/utils/stats.py
@@ -5,3 +5,7 @@ def get_mean_std(I):
 
 def standardize(x, mu, std):
     return (x - mu) / std
+
+def standardize_brightness(x):
+    p = np.percentile(x, 90)
+    return np.clip(x * 255 / p, 0, 255).astype("uint8")
