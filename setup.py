b54c2286239aafe5e370103392d07232d3ff3c21
André Pedersen <andrped94@gmail.com>
2023-01-29 12:39:40 +0100

    fix: updated setup.py to properly support jax during install

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
     extras_require={
         "tf": ["tensorflow"],
         "torch": ["torch"],
+        "jax": ["jax", "jaxlib"],
     },
     classifiers=[
         'Development Status :: 4 - Beta',
