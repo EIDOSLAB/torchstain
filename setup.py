3e10af2299998fe9e2e3e5072294bc08ffebb6c1
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2025-01-13 16:34:45 +0100

    Bump version to 1.4.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='torchstain',
-    version='1.4.0',
+    version='1.4.1',
     description='Stain normalization tools for histological analysis and computational pathology',
     long_description=README,
     long_description_content_type='text/markdown',
